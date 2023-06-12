c9687caa508926b67f8d8353445275c019474833
Peter Verhage <peter@egeniq.com>
2023-06-12 10:33:42 +0200

    Initial setup

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\FormController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes (prefixed with /api/)
+|--------------------------------------------------------------------------
+*/
+
+Route::get('forms', [FormController::class, 'index']);
+Route::get('forms/{id}', [FormController::class, 'show']);
+Route::post('forms/{id}', [FormController::class, 'submit']);
+Route::post('forms/{id}/files', [FormController::class, 'uploadFile']);
