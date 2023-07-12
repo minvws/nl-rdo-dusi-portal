e0fffcb99b9dfec99e424dd76dc7adb30da29afa
Gerbrand Bosch <gerbrandb@gmail.com>
2023-07-12 06:52:50 +0200

    Updated init script

--- init.sh
+++ init.sh
@@ -12,5 +12,5 @@ vendor/bin/sail up -d --remove-orphans
 vendor/bin/sail artisan key:generate
 
 # Install gmp to accellerate encryption/decryption
-docker-compose exec -it nl-rdo-dusi-portal-web-laravel.test-1 /bin/bash
 vendor/bin/sail root-shell -c 'apt-get update; apt-get install php8.2-gmp'
+vendor/bin/sail restart
