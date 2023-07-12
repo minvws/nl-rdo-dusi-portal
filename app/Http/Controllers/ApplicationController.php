f841d92fcf313760ea3dca79dfc8c6a983fd9862
Peter Verhage <peter@egeniq.com>
2023-07-12 12:09:19 +0200

    Fixed feedback

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ApplicationSubmitRequest;
 use App\Http\Requests\ApplicationUploadFileRequest;
 use App\Models\Application;
-use App\Models\DraftApplication;
 use App\Models\FormData;
 use App\Services\ApplicationService;
 use App\Services\Exceptions\FormNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Routing\ResponseFactory;
@@ -33,9 +31,9 @@ class ApplicationController extends Controller
      */
     public function uploadFile(Application $application, ApplicationUploadFileRequest $request, ApplicationService $applicationService): JsonResponse
     {
-        $fieldCode = $request->safe()->{'fieldCode'};
+        $fieldCode = $request->safe()['fieldCode'];
         assert(is_string($fieldCode));
-        $file = $request->safe()->{'file'};
+        $file = $request->safe()['file'];
         assert($file instanceof UploadedFile);
         $id = $applicationService->uploadFile($application, $fieldCode, $file);
         return response()->json(['id' => $id], status: 202);
@@ -43,7 +41,7 @@ class ApplicationController extends Controller
 
     public function submit(Application $application, ApplicationSubmitRequest $request, ApplicationService $applicationService): Response|ResponseFactory
     {
-        $encryptedData = $request->safe()->{'data'};
+        $encryptedData = $request->safe()['data'];
         assert(is_string($encryptedData));
         $applicationService->submit($application, $encryptedData);
         return response(status: 202);
--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\FormData;
-use App\Services\FormService;
 use Illuminate\Http\JsonResponse;
 
 class FormController extends Controller
 {
-    public function show(FormData $form, FormService $formService): JsonResponse
+    public function show(FormData $form): JsonResponse
     {
         return JsonResponse::fromJsonString($form->json);
     }
