796d44db8edfda39503f33251b1ef774ad880af4
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-07-12 09:16:47 +0200

    Add coverage report (#14)
    * Add coverage report
    * Add phpunit configuration file
    * Update workflow
    * Update test
    * Add testing app key to phpunit.xml.dist

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,12 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_redirects_to_login_screen(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
     }
 }
