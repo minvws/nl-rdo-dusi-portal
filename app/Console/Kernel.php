4fe8e89854b03f769fe1931f040e080ef9ac9ce1
thomas-samoht <54805624+thomas-samoht@users.noreply.github.com>
2023-07-13 09:51:45 +0200

    Ran phpcbf

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
--- tests/Feature/Repositories/SubsidyRepositoryTest.php
+++ tests/Feature/Repositories/SubsidyRepositoryTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Repositories;
 
-use App\Models\Connection;
 use App\Models\Enums\VersionStatus;
 use App\Models\Form;
 use App\Models\Subsidy;
