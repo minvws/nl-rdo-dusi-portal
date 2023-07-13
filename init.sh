3eaf913431f638f6e0da069ce20702dc032d6238
gerbrand-bosch <12494166+gerbrand-bosch@users.noreply.github.com>
2023-07-13 10:20:50 +0200

    Updated init and run scripts (#33)
    * Updated init and run scripts
    
    * Added lint script to run linting locally

--- init.sh
+++ init.sh
@@ -4,22 +4,10 @@ set -e
 if [ ! -f ".env" ]; then
     cp .env.example .env
 fi
-if [ ! -f ".env.user" ]; then
-    cp .env.user.example .env.user
-fi
-source .env
 
 composer install
 npm install
 npm run build
-bash -c "cd $USER_APP_PATH && composer install"
 
 vendor/bin/sail up -d --remove-orphans
-# vendor/bin/sail artisan key:generate
-
-# docker-compose exec user-admin-web php artisan key:generate
-docker-compose exec user-admin-web php artisan migrate
-
-docker-compose exec user-admin-web php artisan migrate:fresh
-docker-compose exec user-admin-web php artisan user:create mail@example.com user password
-echo "Log in with: mail@example.com password"
+vendor/bin/sail artisan key:generate
