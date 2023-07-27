ceff8666f11127fa1675e852fb3b0a22e04dc519
Gerbrand Bosch <gerbrandb@gmail.com>
2023-07-27 17:27:29 +0200

    WIP

--- tests/Repositories/SubsidyRepositoryTest.php
+++ tests/Repositories/SubsidyRepositoryTest.php
@@ -16,17 +16,42 @@ use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyStage;
 use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyVersion;
 use Orchestra\Testbench\TestCase;
 use function PHPUnit\Framework\assertNotNull;
+use Illuminate\Contracts\Config\Repository;
 
 
 class SubsidyRepositoryTest extends TestCase
 {
     use DatabaseMigrations;
 
-    protected array $connectionsToTransact = [Connection::FORM];
+//    protected array $connectionsToTransact = [Connection::FORM];
+
+    protected function defineEnvironment($app): void
+    {
+        // Setup default database to use sqlite :memory:
+        tap($app->make('config'), function (Repository $config) {
+            $config->set('database.default', 'pgsql_form');
+            $config->set('database.connections.pgsql_form', [
+                'driver' => 'pgsql',
+                'url' => env('DATABASE_FORM_URL'),
+                'host' => env('DB_FORM_HOST', '127.0.0.1'),
+                'port' => env('DB_FORM_PORT', '5432'),
+                'database' => env('DB_FORM_DATABASE', 'forge'),
+                'username' => env('DB_FORM_USERNAME', 'forge'),
+                'password' => env('DB_FORM_PASSWORD', ''),
+                'charset' => 'utf8',
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'search_path' => 'public',
+                'sslmode' => 'prefer',
+            ]);
+        });
+    }
 
     public function testGetSubsidy(): void
     {
         $subsidy = Subsidy::factory()->create();
+        dd(config('database'));
+
         $subsidyVersion = SubsidyVersion::factory()->create(
             [
                 'status' => VersionStatus::Published,
