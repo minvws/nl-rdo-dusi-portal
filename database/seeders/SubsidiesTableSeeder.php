2df7b6aa3cbedfb3be5401c72775cb7995f13bf7
Gerbrand Bosch <gerbrandb@gmail.com>
2023-08-02 09:35:08 +0200

    Added more fake subsidies

--- database/seeders/SubsidiesTableSeeder.php
+++ database/seeders/SubsidiesTableSeeder.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\DB;
 class SubsidiesTableSeeder extends Seeder
 {
     public const BTV_UUID = '00f26400-7232-475f-922c-6b569b7e421a';
-    //TODO: Meer subsidies
     /**
      * Run the database seeds.
      */
@@ -19,6 +18,20 @@ class SubsidiesTableSeeder extends Seeder
             'title' => 'Borstprothesen transvrouwen',
             'description' => "Transvrouwen zijn man-vrouw transgenders die negatieve gevoelens ('genderdysforie') ervaren omdat ze als man geboren zijn en in transitie zijn om als vrouw te leven. De meerderheid van de transvrouwen vindt, ook na behandeling (de zogeheten genderbevestigende hormonale therapie), dat zij te weinig borstweefsel heeft voor een vrouwelijk profiel. Dit kan een grote hindernis zijn bij de transitie. Een borstvergroting kan deze hinder verminderen.",
             'valid_from' => '2019-02-01',
+            'valid_to' => null
+        ]);
+        DB::table('subsidies')->insert([
+            'id' => fake()->uuid(),
+            'title' => 'Alternative subsidie',
+            'description' => "Transvrouwen zijn man-vrouw transgenders die negatieve gevoelens ('genderdysforie') ervaren omdat ze als man geboren zijn en in transitie zijn om als vrouw te leven. De meerderheid van de transvrouwen vindt, ook na behandeling (de zogeheten genderbevestigende hormonale therapie), dat zij te weinig borstweefsel heeft voor een vrouwelijk profiel. Dit kan een grote hindernis zijn bij de transitie. Een borstvergroting kan deze hinder verminderen.",
+            'valid_from' => '2020-02-01',
+            'valid_to' => null
+        ]);
+        DB::table('subsidies')->insert([
+            'id' => fake()->uuid(),
+            'title' => 'Subsidie die niet bestaat',
+            'description' => "Transvrouwen zijn man-vrouw transgenders die negatieve gevoelens ('genderdysforie') ervaren omdat ze als man geboren zijn en in transitie zijn om als vrouw te leven. De meerderheid van de transvrouwen vindt, ook na behandeling (de zogeheten genderbevestigende hormonale therapie), dat zij te weinig borstweefsel heeft voor een vrouwelijk profiel. Dit kan een grote hindernis zijn bij de transitie. Een borstvergroting kan deze hinder verminderen.",
+            'valid_from' => '2021-02-01',
             'valid_to' => null
         ]);
     }
