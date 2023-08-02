26e69b4af4b82288a8c85c0587f924b8859c50c6
Gerbrand Bosch <gerbrandb@gmail.com>
2023-08-02 09:53:06 +0200

    Added more fake subsidies

--- database/seeders/SubsidiesTableSeeder.php
+++ database/seeders/SubsidiesTableSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use MinVWS\DUSi\Shared\Subsidy\Models\Enums\VersionStatus;
+use MinVWS\DUSi\Shared\Subsidy\Models\Subsidy;
+use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyStage;
+use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyVersion;
 
 class SubsidiesTableSeeder extends Seeder
 {
@@ -20,19 +24,14 @@ class SubsidiesTableSeeder extends Seeder
             'valid_from' => '2019-02-01',
             'valid_to' => null
         ]);
-        DB::table('subsidies')->insert([
-            'id' => fake()->uuid(),
-            'title' => 'Alternative subsidie',
-            'description' => "Transvrouwen zijn man-vrouw transgenders die negatieve gevoelens ('genderdysforie') ervaren omdat ze als man geboren zijn en in transitie zijn om als vrouw te leven. De meerderheid van de transvrouwen vindt, ook na behandeling (de zogeheten genderbevestigende hormonale therapie), dat zij te weinig borstweefsel heeft voor een vrouwelijk profiel. Dit kan een grote hindernis zijn bij de transitie. Een borstvergroting kan deze hinder verminderen.",
-            'valid_from' => '2020-02-01',
-            'valid_to' => null
-        ]);
-        DB::table('subsidies')->insert([
-            'id' => fake()->uuid(),
-            'title' => 'Subsidie die niet bestaat',
-            'description' => "Transvrouwen zijn man-vrouw transgenders die negatieve gevoelens ('genderdysforie') ervaren omdat ze als man geboren zijn en in transitie zijn om als vrouw te leven. De meerderheid van de transvrouwen vindt, ook na behandeling (de zogeheten genderbevestigende hormonale therapie), dat zij te weinig borstweefsel heeft voor een vrouwelijk profiel. Dit kan een grote hindernis zijn bij de transitie. Een borstvergroting kan deze hinder verminderen.",
-            'valid_from' => '2021-02-01',
-            'valid_to' => null
-        ]);
+        Subsidy::factory()->count(2)
+            ->create()
+            ->each(function($subsidy){
+                SubsidyVersion::factory(['subsidy_id' => $subsidy->id, 'status' => VersionStatus::Published])
+                    ->create()
+                    ->each(function($subsidyVersion){
+                    SubsidyStage::factory(['subsidy_version_id' => $subsidyVersion->id])->create();
+                });
+            });
     }
 }
