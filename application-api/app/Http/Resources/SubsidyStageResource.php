60944c701c2b56a8aa6925dc30fb585c8a853265
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-08-30 10:10:20 +0200

    Frontend validation (#107)
    * Added extra frontend schema options
    
    * Added extra frontend schema options
    
    * Ignore CyclomaticComplexity for now

--- application-api/app/Http/Resources/SubsidyStageResource.php
+++ application-api/app/Http/Resources/SubsidyStageResource.php
@@ -67,37 +67,65 @@ class SubsidyStageResource extends JsonResource
         return $result;
     }
 
+    /**
+     * @param array $result
+     * @param Field $field
+     * @return void
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
     private function addFieldOptionsForType(array &$result, Field $field): void
     {
         switch ($field->type) {
-            case FieldType::Select:
-                $result['enum'] = $field->params['options'];
-                break;
-            case FieldType::Upload:
-                $result['file'] = true;
+            case FieldType::Checkbox:
+                $result['const'] = true;
                 break;
             case FieldType::CustomBankAccount:
                 $result['iban'] = true;
                 break;
-            case FieldType::TextTel:
-                $result['tel'] = true;
+            case FieldType::CustomCountry:
+                // Currently nothing extra
                 break;
-            case FieldType::Checkbox:
-                $result['const'] = true;
-                break;
-            case FieldType::TextEmail:
-                $result['format'] = 'email';
+            case FieldType::CustomPostalCode:
+                // Currently nothing extra
                 break;
             case FieldType::Date:
                 $result['format'] = 'date';
                 break;
-            default:
+            case FieldType::Multiselect:
+                // Currently nothing extra
+                break;
+            case FieldType::Select:
+                $result['enum'] = $field->params['options'];
+                break;
+            case FieldType::Text:
+                $result = array_merge($result, $this->getStringValidationOptions($field));
+                break;
+            case FieldType::TextArea:
+                $result = array_merge($result, $this->getStringValidationOptions($field));
+                break;
+            case FieldType::TextEmail:
+                $result['format'] = 'email';
+                $result = array_merge($result, $this->getStringValidationOptions($field));
+                break;
+            case FieldType::TextTel:
+                $result['tel'] = true;
+                $result = array_merge($result, $this->getStringValidationOptions($field));
+                break;
+            case FieldType::TextNumeric:
+                $result['minimum'] = 0;
+                $result = array_merge($result, $this->getNumberValidationOptions($field));
+                break;
+            case FieldType::TextUrl:
+                // Currently nothing extra
+                break;
+            case FieldType::Upload:
+                $result['file'] = true;
+                break;
         }
     }
 
     private function createFieldDataSchema(Field $field): array
     {
-
         $type = match ($field->type) {
             FieldType::TextNumeric => 'integer',
             FieldType::Checkbox => 'boolean',
@@ -109,10 +137,6 @@ class SubsidyStageResource extends JsonResource
             'title' => $field->title,
         ];
 
-        if ($type === 'integer') {
-            $result['minimum'] = 0;
-        }
-
         if (!empty($field->description)) {
             $result['description'] = $field->description;
         }
@@ -120,5 +144,66 @@ class SubsidyStageResource extends JsonResource
         $this->addFieldOptionsForType($result, $field);
 
         return $result;
+    }
+
+    /**
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema4.ts
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema7.ts
+     * @param Field $field
+     * @return array
+     */
+    protected function getNumberValidationOptions(Field $field): array
+    {
+        return array_filter([
+            'multipleOf' => $field->params['multipleOf'] ?? null,
+            'maximum' => $field->params['maximum'] ?? null,
+            'exclusiveMaximum' => $field->params['exclusiveMaximum'] ?? null,
+            'minimum' => $field->params['minimum'] ?? null,
+            'exclusiveMinimum' => $field->params['exclusiveMinimum'] ?? null,
+        ]);
+    }
+
+    /**
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema4.ts
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema7.ts
+     * @param Field $field
+     * @return array
+     */
+    protected function getStringValidationOptions(Field $field): array
+    {
+        return array_filter([
+            'maxLength' => $field->params['maxLength'] ?? null,
+            'minLength' => $field->params['minLength'] ?? null,
+            'pattern' => $field->params['pattern'] ?? null,
+        ]);
+    }
+
+    /**
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema4.ts
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema7.ts
+     * @param Field $field
+     * @return array
+     */
+    protected function getArrayValidationOptions(Field $field): array
+    {
+        return array_filter([
+            'maxItems' => $field->params['maxItems'] ?? null,
+            'minItems' => $field->params['minItems'] ?? null,
+            'uniqueItems' => $field->params['uniqueItems'] ?? null,
+        ]);
+    }
+
+    /**
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema4.ts
+     * @see https://github.com/eclipsesource/jsonforms/blob/master/packages/core/src/models/jsonSchema7.ts
+     * @param Field $field
+     * @return array
+     */
+    protected function getObjectValidationOptions(Field $field): array
+    {
+        return array_filter([
+            'maxProperties' => $field->params['maxProperties'] ?? null,
+            'minProperties' => $field->params['minProperties'] ?? null,
+        ]);
     }
 }
