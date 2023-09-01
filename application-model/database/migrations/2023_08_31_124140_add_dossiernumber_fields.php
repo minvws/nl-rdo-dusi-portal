9e789457df016b9504081d05b38ba1a1eab5f695
Maurice Maas <maurice@egeniq.com>
2023-09-01 13:12:58 +0200

    DUS-105: Ading reference and reference_prefix fields

---
+++ application-model/database/migrations/2023_08_31_124140_add_dossiernumber_fields.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('reference', 6)->after('subsidy_version_id')->unique();
+        });
+
+        Schema::table('subsidies', function (Blueprint $table) {
+            $table->string('reference_prefix', 15)->after('title')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('reference');
+        });
+
+        Schema::table('subsidies', function (Blueprint $table) {
+            $table->dropColumn('reference_prefix');
+        });
+    }
+};
