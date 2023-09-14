d5f4d5a3078c502d07aa4e00dcd62b2b6f36bddb
Peter Verhage <peter@egeniq.com>
2023-09-14 22:10:45 +0200

    Split feature and unit test cases

--- application-backend/tests/Feature/Repositories/ApplicationFileRepositoryTest.php
+++ application-backend/tests/Feature/Repositories/ApplicationFileRepositoryTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Application\Backend\Tests\Feature\Repositories;
 
-use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
 use MinVWS\DUSi\Application\Backend\Tests\TestCase;
@@ -19,59 +18,6 @@ use MinVWS\DUSi\Shared\Subsidy\Models\Field;
 class ApplicationFileRepositoryTest extends TestCase
 {
     use WithFaker;
-
-    public function testRepositoryCanBeCreated(): void
-    {
-        $fileSystem = $this->createMock(Filesystem::class);
-
-        $repository = new ApplicationFileRepository($fileSystem);
-
-        $this->assertInstanceOf(ApplicationFileRepository::class, $repository);
-    }
-
-    public function testRepositoryCanCallWriteFile(): void
-    {
-        $applicationStage = new ApplicationStage();
-        $applicationStage->id = '1';
-
-        $field = new Field();
-        $field->code = '2';
-
-        $id = '3';
-
-        $fileSystem = $this->createMock(Filesystem::class);
-        $fileSystem->expects($this->once())
-            ->method('put')
-            ->with("1/2/3", 'contents')
-            ->willReturn(true);
-
-        $repository = new ApplicationFileRepository($fileSystem);
-        $result = $repository->writeFile($applicationStage, $field, $id, 'contents');
-
-        $this->assertTrue($result);
-    }
-
-    public function testRepositoryCanCallExists(): void
-    {
-        $applicationStage = new ApplicationStage();
-        $applicationStage->id = '2';
-
-        $field = new Field();
-        $field->code = '3';
-
-        $id = '4';
-
-        $fileSystem = $this->createMock(Filesystem::class);
-        $fileSystem->expects($this->once())
-            ->method('exists')
-            ->with("2/3/4")
-            ->willReturn(true);
-
-        $repository = new ApplicationFileRepository($fileSystem);
-        $result = $repository->fileExists($applicationStage, $field, $id, 'contents');
-
-        $this->assertTrue($result);
-    }
 
     public function testUnlinkUnusedFiles(): void
     {
