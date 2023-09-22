9cada8a2d12dd628d3c34c2b0eb9ea6e495e5975
Peter Verhage <peter@egeniq.com>
2023-09-22 09:54:06 +0200

    Fix accidental commit

--- assessment-api/app/Providers/RouteServiceProvider.php
+++ assessment-api/app/Providers/RouteServiceProvider.php
@@ -31,7 +31,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware(['api'])
+            Route::middleware(['api', 'auth'])
                 ->prefix('api')
                 ->as('api.')
                 ->group(base_path('routes/api.php'));
