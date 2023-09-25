dcb0e15da3622eb1832b669c8794df1c66d79eee
gerbrand-bosch <12494166+gerbrand-bosch@users.noreply.github.com>
2023-09-25 23:39:20 +0200

    Created first audit logging event (#382)
    * Created first audit logging event
    
    * Update assessment-api/.env.example
    
    Co-authored-by: Rick Lambrechts <rick@rl-webdiensten.nl>
    
    ---------
    
    Co-authored-by: Rick Lambrechts <rick@rl-webdiensten.nl>

--- assessment-api/app/Services/ApplicationSubsidyService.php
+++ assessment-api/app/Services/ApplicationSubsidyService.php
@@ -5,22 +5,33 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Assessment\API\Services;
 
+use Auth;
+use MinVWS\DUSi\Assessment\API\DTO\Logging\Events\ViewAssignmentEvent;
 use MinVWS\DUSi\Assessment\API\Http\Resources\ApplicationSubsidyVersionResource;
 use MinVWS\DUSi\Shared\Application\Models\Application;
 use MinVWS\DUSi\Shared\Application\Services\ApplicationDataService;
 use MinVWS\DUSi\Shared\Subsidy\Helpers\SubsidyStageDataSchemaBuilder;
+use MinVWS\Logging\Laravel\LogService;
 
 readonly class ApplicationSubsidyService
 {
     public function __construct(
         private ApplicationDataService $applicationDataService,
-        private SubsidyStageDataSchemaBuilder $dataSchemaBuilder
+        private SubsidyStageDataSchemaBuilder $dataSchemaBuilder,
+        private LogService $logger,
     ) {
     }
 
     public function getApplicationSubsidyResource(
         Application $application
     ): ApplicationSubsidyVersionResource {
+        $this->logger->log((new ViewAssignmentEvent())
+        ->withData([
+            'applicationId' => $application->id,
+            //TODO: Update this to withActor and a loggable user object when user login is finished
+            /** @phpstan-ignore-next-line */
+            'userId' => Auth::User()?->id,
+        ]));
         return new ApplicationSubsidyVersionResource(
             $application,
             $this->applicationDataService,
