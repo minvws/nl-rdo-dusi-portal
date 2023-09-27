0edf3e121c731b29e833af40bf56ebefdba2fc1a
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-09-27 12:37:39 +0200

    Update LatteLetterLoader (#401)
    * Remove ApplicationFileRepository because it is moved to shared package
    
    * Update LatteLetterLoader

--- shared/src/Application/Services/LatteLetterLoaderService.php
+++ shared/src/Application/Services/LatteLetterLoaderService.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Shared\Application\Services;
 
-use Latte\Loader;
-use Latte\RuntimeException;
+use Latte\Loaders\FileLoader;
 use Latte\Strict;
 
-class LatteLetterLoaderService implements Loader
+class LatteLetterLoaderService extends FileLoader
 {
     use Strict;
-
-    protected string $baseDir;
 
     /** @var string[]  [name => content] */
     protected ?array $templates;
@@ -22,6 +19,8 @@ class LatteLetterLoaderService implements Loader
     {
         $this->baseDir = self::normalizePath("$baseDir/");
         $this->templates = $templates;
+
+        parent::__construct($baseDir);
     }
 
     public function addTemplate(string $key, string $content): void
@@ -31,75 +30,10 @@ class LatteLetterLoaderService implements Loader
 
     /**
      * Returns template source code.
+     * @psalm-suppress ParamNameMismatch
      */
-    public function getContent(string $name): string
+    public function getContent(string $fileName): string
     {
-        if (isset($this->templates[$name])) {
-            return $this->templates[$name];
-        }
-
-        $file = $this->baseDir . $name;
-        if ($this->baseDir && !str_starts_with(self::normalizePath($file), $this->baseDir)) {
-            throw new RuntimeException("Template '$file' is not within the allowed path '$this->baseDir'.");
-        }
-
-        if (!is_file($file)) {
-            throw new RuntimeException("Missing template file '$file'.");
-        }
-
-        if (touch($file) === false || $this->isExpired($name, time())) {
-            trigger_error(
-                "File's modification time is in the future. Cannot update it",
-                E_USER_WARNING
-            );
-        }
-
-        $contents = file_get_contents($file);
-
-        if (!$contents) {
-            throw new RuntimeException("Could not get contents of '$file'.");
-        }
-
-        return $contents;
-    }
-
-    public function isExpired(string $name, int $time): bool
-    {
-        $mtime = filemtime($this->baseDir . $name); // @ - stat may fail
-        return !$mtime || $mtime > $time;
-    }
-
-    /**
-     * Returns referred template name.
-     */
-    public function getReferredName(string $name, string $referringName): string
-    {
-        if ($this->baseDir || !preg_match('#/|\\\\|[a-z][a-z0-9+.-]*:#iA', $name)) {
-            $name = self::normalizePath($referringName . '/../' . $name);
-        }
-
-        return $name;
-    }
-
-    /**
-     * Returns unique identifier for caching.
-     */
-    public function getUniqueId(string $name): string
-    {
-        return $this->baseDir . str_replace('/', DIRECTORY_SEPARATOR, $name);
-    }
-
-    protected static function normalizePath(string $path): string
-    {
-        $res = [];
-        foreach (explode('/', str_replace('\\', '/', $path)) as $part) {
-            if ($part === '..' && $res && end($res) !== '..') {
-                array_pop($res);
-            } elseif ($part !== '.') {
-                $res[] = $part;
-            }
-        }
-
-        return implode(DIRECTORY_SEPARATOR, $res);
+        return $this->templates[$fileName] ?? parent::getContent($fileName);
     }
 }
