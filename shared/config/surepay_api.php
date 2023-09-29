d624ade5a60bac445b23e23fce187b7e18668a11
Peter Verhage <peter@egeniq.com>
2023-09-29 10:16:33 +0200

    SurePay proxy config

--- shared/config/surepay_api.php
+++ shared/config/surepay_api.php
@@ -6,6 +6,11 @@ return [
     'key' => env('SUREPAY_KEY'),
     'secret' => env('SUREPAY_SECRET'),
     'endpoint' => env('SUREPAY_ENDPOINT'),
+    'verify_ssl' => env('SUREPAY_VERIFY_SSL', false),
+    'proxy' => [
+        'http' => env('SUREPAY_HTTP_PROXY', ''),
+        'https' => env('SUREPAY_HTTPS_PROXY', ''),
+    ],
     'debug' => env('SUREPAY_DEBUG', false),
     'request_timeout_seconds' => env('SUREPAY_REQUEST_TIMEOUT_SECONDS', 3),
     'connect_timeout_seconds' => env('SUREPAY_CONNECT_TIMEOUT_SECONDS', 3),
