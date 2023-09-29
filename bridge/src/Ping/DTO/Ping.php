35f497be53234944943d9dc151ac15cd76576c7d
Peter Verhage <peter@egeniq.com>
2023-09-29 14:44:59 +0200

    Update bridge to modern Codable support

--- bridge/src/Ping/DTO/Ping.php
+++ bridge/src/Ping/DTO/Ping.php
@@ -1,4 +1,5 @@
-<?php
+<?php // phpcs:disable PSR1.Files.SideEffects
+
 
 declare(strict_types=1);
 
@@ -6,29 +7,17 @@ namespace MinVWS\DUSi\Shared\Bridge\Ping\DTO;
 
 use DateTimeImmutable;
 use MinVWS\Codable\Coding\Codable;
-use MinVWS\Codable\Decoding\Decodable;
-use MinVWS\Codable\Decoding\DecodingContainer;
-use MinVWS\Codable\Encoding\EncodingContainer;
+use MinVWS\Codable\Coding\CodableSupport;
+use MinVWS\Codable\Reflection\Attributes\CodableDateTime;
 
-class Ping implements Codable
+readonly class Ping implements Codable
 {
+    use CodableSupport;
+
     public const DATETIME_FORMAT = 'Y-m-d\TH:i:s.up'; // includes microseconds
 
-    final public function __construct(public readonly DateTimeImmutable $requestStamp)
-    {
-    }
-
-    /**
-     * @SuppressWarnings(UnusedFormalParameter)
-     */
-    public static function decode(DecodingContainer $container, ?Decodable $object = null): static
-    {
-        $stamp = $container->nestedContainer('requestStamp')->decodeDateTime(self::DATETIME_FORMAT);
-        return new static($stamp);
-    }
-
-    public function encode(EncodingContainer $container): void
-    {
-        $container->{'requestStamp'}->encodeDateTime($this->requestStamp, self::DATETIME_FORMAT);
+    final public function __construct(
+        #[CodableDateTime(format: self::DATETIME_FORMAT)] public DateTimeImmutable $requestStamp
+    ) {
     }
 }
