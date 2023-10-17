d0007c09f1f33fcb8767cb6710560372c01fa047
Laurence Keijmel <laurence@egeniq.com>
2023-10-17 16:29:59 +0200

    Update to application stage transitions response

--- assessment-api/app/Http/Resources/ApplicationStageTransitionResource.php
+++ assessment-api/app/Http/Resources/ApplicationStageTransitionResource.php
@@ -4,22 +4,28 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Assessment\API\Http\Resources;
 
+use DateTimeInterface;
 use Faker\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use DateTime;
+use MinVWS\DUSi\Shared\Application\Models\Application;
+use MinVWS\DUSi\Shared\Application\Models\ApplicationMessage;
 use MinVWS\DUSi\Shared\Application\Models\ApplicationStage;
 use MinVWS\DUSi\Shared\Serialisation\Models\Application\ApplicationStatus;
 use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyStageTransition;
 
 /**
- * @property string $id
- * @property SubsidyStageTransition $subsidyStageTransition
- * @property ApplicationStage $previousApplicationStage
- * @property ApplicationStage $newApplicationStage
- * @property ApplicationStatus $previous_application_status
- * @property ApplicationStatus $new_application_status
- * @property DateTime $created_at
+ * @property string                      $id
+ * @property string                      $description
+ * @property string                      $application_id
+ * @property DateTimeInterface           $created_at
+ * @property-read Application            $application
+ * @property-read SubsidyStageTransition $subsidyStageTransition
+ * @property-read ApplicationStage       $previousApplicationStage
+ * @property-read ?ApplicationStage      $newApplicationStage
+ * @property-read ApplicationStatus      $previous_application_status
+ * @property-read ApplicationStatus      $new_application_status
+ * @property-read ?ApplicationMessage    $applicationMessage
  */
 class ApplicationStageTransitionResource extends JsonResource
 {
@@ -32,23 +38,35 @@ class ApplicationStageTransitionResource extends JsonResource
     public function toArray(Request $request): array
     {
         $faker = Factory::create();
-        return [
+        $data = [
             'id' => $this->id,
-            'description' => $faker->title,
+            'internalNote' => $faker->text,
+            'createdAt' => $this->created_at,
+            'previousApplicationStatus' => $this->previous_application_status,
+            'newApplicationStatus' => $this->new_application_status,
+            'subsidyStageTransition' => $this->subsidyStageTransition->only(['id', 'description']),
             'previousApplicationStage' => [
                 'subsidyStage' => $this->previousApplicationStage->subsidyStage?->only(['id', 'title', 'stage']),
                 'assessorUser' => $this->previousApplicationStage->assessorUser?->only(['id', 'name']),
             ],
-            'newApplicationStage' => [
+        ];
+
+        if ($this->newApplicationStage) {
+            $data['newApplicationStage'] = [
                 'subsidyStage' => $this->newApplicationStage->subsidyStage?->only(['id', 'title', 'stage']),
                 'assessorUser' => $this->newApplicationStage->assessorUser?->only(['id', 'name']),
-            ],
-            'message' => [
-                'id' => $faker->uuid,
-                'subject' => sprintf('Bericht %s', $faker->title),
-            ],
-            'internalNote' => $faker->text,
-            'createdAt' => $this->created_at,
-        ];
+            ];
+        }
+
+        if ($this->applicationMessage) {
+            $data['message'] = [
+                'id' => $this->applicationMessage->id,
+                'subject' => $this->applicationMessage->subject,
+                'pdf' => $this->applicationMessage->pdf_path,
+                'html' => $this->applicationMessage->html_path,
+            ];
+        }
+
+        return $data;
     }
 }
