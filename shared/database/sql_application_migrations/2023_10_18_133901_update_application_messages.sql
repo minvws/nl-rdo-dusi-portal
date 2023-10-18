4165cfbf2efca7417d5eb12284f89b0e568e1181
Peter Verhage <peter@egeniq.com>
2023-10-18 13:48:26 +0200

    SQL migrations

---
+++ shared/database/sql_application_migrations/2023_10_18_133901_update_application_messages.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+UPDATE application_messages m
+SET application_stage_transition_id = (
+    SELECT t.id
+    FROM application_stage_transitions t
+    WHERE t.created_at <= m.sent_at
+    AND t.application_id = m.application_id
+    ORDER BY t.created_at DESC
+    LIMIT 1
+)
+WHERE m.application_stage_transition_id IS NULL;
+
+COMMIT;
