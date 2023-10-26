d5be480bfa5175de3a04646eb47283f138eefbc8
Herman van Rink <rink@initfour.nl>
2023-10-26 23:47:28 +0200

    Rework table privileges dusi_user_db

---
+++ shared/database/sql/dusi_user_db/v1.3.0/grants.sql
@@ -0,0 +1,50 @@
+
+-- Handle with care, execute manually:
+-- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM user_admin_dusi;
+-- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM assessment_web_dusi;
+-- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM backend_dusi;
+
+-- Cleanup alternate from with dashes
+-- DROP ROLE IF EXISTS "dpw-dusi";
+-- DROP ROLE IF EXISTS "backend-dusi";
+-- DROP ROLE IF EXISTS "user-admin-dusi";
+-- DROP ROLE IF EXISTS "assessment-web-dusi";
+-- 
+-- CREATE ROLE "dpw_dusi" WITH LOGIN;
+-- CREATE ROLE "backend_dusi" WITH LOGIN;
+-- CREATE ROLE "user_admin_dusi" WITH LOGIN;
+-- CREATE ROLE "assessment_web_dusi" WITH LOGIN;
+
+ALTER TABLE public.migrations OWNER TO dusi_dba;
+ALTER TABLE public.organisations OWNER TO dusi_dba;
+ALTER TABLE public.role_user OWNER TO dusi_dba;
+ALTER TABLE public.roles OWNER TO dusi_dba;
+ALTER TABLE public.users OWNER TO dusi_dba;
+ALTER TABLE public.deploy_releases OWNER TO dusi_dba;
+
+GRANT SELECT ON TABLE public.deploy_releases TO "user-admin-dusi";
+GRANT SELECT ON TABLE public.deploy_releases TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.deploy_releases TO "backend_dusi";
+
+GRANT SELECT ON TABLE public.migrations TO "user_admin_dusi";
+GRANT SELECT ON TABLE public.migrations TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.migrations TO "backend_dusi";
+
+GRANT SELECT,INSERT,UPDATE ON TABLE public.organisations TO "user_admin_dusi";
+GRANT SELECT ON TABLE public.organisations TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.organisations TO "backend_dusi";
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON TABLE public.role_user TO "user_admin_dusi";
+GRANT SELECT ON TABLE public.role_user TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.role_user TO "backend_dusi";
+
+GRANT SELECT ON TABLE public.roles TO "user_admin_dusi";
+GRANT SELECT ON TABLE public.roles TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.roles TO "backend_dusi";
+
+GRANT SELECT,INSERT,UPDATE ON TABLE public.users TO "user_admin_dusi";
+GRANT SELECT ON TABLE public.users TO "assessment_web_dusi";
+GRANT UPDATE (password) ON TABLE public.users TO "assessment_web_dusi";
+GRANT SELECT ON TABLE public.users TO "backend_dusi";
+
+
