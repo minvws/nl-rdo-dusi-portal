23fbf09bbb448e1532b33b026175fc83ac53eaee
Maurice Maas <mjgmaas@gmail.com>
2023-12-18 16:19:19 +0100

    Update shared/database/migrations/2023_11_28_142301_add_suggestion_column_to_surepay_results.php
    Co-authored-by: Rick Lambrechts <rick@rl-webdiensten.nl>

--- shared/database/migrations/2023_11_28_142301_add_suggestion_column_to_surepay_results.php
+++ shared/database/migrations/2023_11_28_142301_add_suggestion_column_to_surepay_results.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('application_surepay_results', function (Blueprint $table) {
-            $table->string('encrypted_name_suggestion', 1000)->after('country_code')->nullable();
+            $table->text('encrypted_name_suggestion', 1000)->after('country_code')->nullable();
         });
     }
 
