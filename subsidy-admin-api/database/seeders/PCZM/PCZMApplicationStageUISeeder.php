c26dcd92c701115c64e15a2e939927345f11a5cf
Maurice Maas <maurice@egeniq.com>
2023-12-19 12:14:31 +0100

    DUSI-179: Abstraction

--- subsidy-admin-api/database/seeders/PCZM/PCZMApplicationStageUISeeder.php
+++ subsidy-admin-api/database/seeders/PCZM/PCZMApplicationStageUISeeder.php
@@ -4,87 +4,18 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Subsidy\Admin\API\Database\Seeders\PCZM;
 
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use MinVWS\DUSi\Subsidy\Admin\API\Database\Seeders\AbstractApplicationStageUISeeder;
 use stdClass;
 
-class PCZMApplicationStageUISeeder extends Seeder
+class PCZMApplicationStageUISeeder extends AbstractApplicationStageUISeeder
 {
     public const PCZM_STAGE1_V1_UUID = 'e6d5cd35-8c67-40c4-abc4-b1d6bf8afb97';
 
-    private function resolveFileReferencesInArray(array $array, string $basePath): void
-    {
-        foreach ($array as $value) {
-            if ($value instanceof stdClass) {
-                $this->resolveFileReferences($value, $basePath);
-            } elseif (is_array($value)) {
-                $this->resolveFileReferencesInArray($value, $basePath);
-            }
-        }
-    }
-    private function resolveFileReferences(stdClass $object, string $basePath): void
-    {
-        foreach (get_object_vars($object) as $key => $value) {
-            if ($value instanceof stdClass) {
-                $this->resolveFileReferences($value, $basePath);
-            } elseif (is_array($value)) {
-                $this->resolveFileReferencesInArray($value, $basePath);
-            } elseif (is_string($value) && str_starts_with($value, 'file:')) {
-                $path = $basePath . DIRECTORY_SEPARATOR . substr($value, 5);
-                $object->$key = file_get_contents($path);
-            }
-        }
-    }
-
-    private function loadInputUiStepForStage(int $stage, int $step): stdClass
-    {
-        $json = file_get_contents(__DIR__ . sprintf('/resources/input_ui/stage%d_step%d.json', $stage, $step));
-        assert(is_string($json));
-        $stageStepData = json_decode($json);
-        assert($stageStepData instanceof stdClass);
-        $this->resolveFileReferences($stageStepData, __DIR__ . '/resources/input_ui');
-        return $stageStepData;
-    }
-
-    private function loadAllOfStepForStage(int $stage, int $step): array
-    {
-        $stepFilePath = __DIR__ . sprintf('/resources/input_ui/stage%d_step%d_allOf.json', $stage, $step);
-        if (!file_exists($stepFilePath)) {
-            return [];
-        }
-        $json = file_get_contents($stepFilePath);
-        assert(is_string($json));
-        $allOf = json_decode($json);
-
-        return $allOf;
-    }
-
-    private function buildInputUiStep(int $step, string $label, array $required): stdClass
-    {
-        return (object)[
-            'type' => 'CustomPageControl',
-            'label' => $label,
-            'elements' => [$this->loadInputUiStepForStage(1, $step)],
-            'options' => (object)[
-                'required' => $required,
-                'allOf' => $this->loadAllOfStepForStage(1, $step)
-            ]
-        ];
-    }
-
-    public function buildViewSchema(): array
-    {
-        $filePath = __DIR__ . '/resources/view_ui/stage1.json';
-        if (!file_exists($filePath)) {
-            return [];
-        }
-        $json = file_get_contents($filePath);
-        assert(is_string($json));
-        return json_decode($json, true);
-    }
-
     public function run(): void
     {
+        $this->resourceDir = __DIR__;
+
         $inputUi = $this->buildInputUi();
         $viewUi = $this->buildViewSchema();
 
