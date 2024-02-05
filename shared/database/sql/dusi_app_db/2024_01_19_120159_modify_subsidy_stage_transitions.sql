7eaa45ca9b06cac5c1e551166f07c20ec09cc025
Peter Verhage <peter@egeniq.com>
2024-02-05 14:56:56 +0100

    Pull request feedback / CI fixes

--- shared/database/sql/dusi_app_db/2024_01_19_120159_modify_subsidy_stage_transitions.sql
+++ shared/database/sql/dusi_app_db/2024_01_19_120159_modify_subsidy_stage_transitions.sql
@@ -1,5 +1,5 @@
 ALTER TABLE public.subsidy_stage_transitions
     DROP CONSTRAINT subsidy_stage_transitions_current_subsidy_stage_id_target_subsi,
-    ADD COLUMN expiration_duration INTEGER NULL,
+    ADD COLUMN expiration_period INTEGER NULL,
     ADD COLUMN evaluation_trigger VARCHAR(255) NOT NULL DEFAULT 'submit',
     ADD CONSTRAINT "subsidy_stage_transitions_evaluation_trigger_check" CHECK (evaluation_trigger IN ('submit', 'expiration'));
