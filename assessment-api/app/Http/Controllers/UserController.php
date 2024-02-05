f3dda9ad28f12f86a910f2f5534712d39d2854c9
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-02-05 14:52:01 +0100

    chore(assessment-user): update roles in user information endpoint (#1173)

--- assessment-api/app/Http/Controllers/UserController.php
+++ assessment-api/app/Http/Controllers/UserController.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace MinVWS\DUSi\Assessment\API\Http\Controllers;
 
+use Illuminate\Support\Collection;
+use MinVWS\DUSi\Assessment\API\Http\Resources\UserResource;
 use MinVWS\DUSi\Shared\Subsidy\Models\Subsidy;
 use MinVWS\DUSi\Shared\User\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use MinVWS\DUSi\Assessment\API\Http\Resources\SubsidyResource;
-use MinVWS\DUSi\Shared\User\Resources\UserResource;
 use MinVWS\DUSi\Shared\User\Services\UserService;
 
 class UserController extends Controller
@@ -21,7 +22,10 @@ class UserController extends Controller
 
     public function show(Request $request): UserResource
     {
-        return UserResource::make($request->user());
+        /** @var User $user */
+        $user = $request->user();
+
+        return new UserResource($user, $this->getSubsidiesForUser($user));
     }
 
     public function subsidies(Request $request): ResourceCollection
@@ -29,10 +33,15 @@ class UserController extends Controller
         /** @var User $user */
         $user = $request->user();
 
+        return SubsidyResource::collection($this->getSubsidiesForUser($user));
+    }
+
+    protected function getSubsidiesForUser(User $user): Collection
+    {
         if ($this->userService->hasAccessToAllSubsidies($user)) {
-            return SubsidyResource::collection(Subsidy::all());
+            return Subsidy::all();
         }
 
-        return SubsidyResource::collection($this->userService->getSubsidiesForUser($user));
+        return $this->userService->getSubsidiesForUser($user);
     }
 }
