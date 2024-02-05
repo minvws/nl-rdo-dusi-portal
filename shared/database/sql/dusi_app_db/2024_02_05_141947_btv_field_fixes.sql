12ebde040fd9e82af3963cffb1772fba57c05027
Maurice Maas <maurice@egeniq.com>
2024-02-05 14:36:07 +0100

    DUSI-1169: Also remove answers for field internalAssessmentReasonForRejection

--- shared/database/sql/dusi_app_db/2024_02_05_141947_btv_field_fixes.sql
+++ shared/database/sql/dusi_app_db/2024_02_05_141947_btv_field_fixes.sql
@@ -1,5 +1,12 @@
+delete
+from public.answers
+where field_id = (select id
+                  from public.fields
+                  where code = 'internalAssessmentReasonForRejection' and subsidy_stage_id = 'e456e790-1919-4a2b-b3d5-337d0053abe3');
+
 delete from public.fields
 where code = 'internalAssessmentReasonForRejection' and subsidy_stage_id = 'e456e790-1919-4a2b-b3d5-337d0053abe3';
+
 
 UPDATE public.fields
 SET
