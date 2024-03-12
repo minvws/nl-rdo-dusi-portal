0b91d2c2adcad4956655e5fc9089b4fc17c3e986
Maurice Maas <maurice@egeniq.com>
2024-03-12 10:48:31 +0100

    DUSI-636: DAMU subsidy SQL migration

---
+++ shared/database/sql/dusi_app_db/2024_03_10_142701_modify_field_type.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.fields
+    ADD CONSTRAINT fields_type_check check ("type" in ('text', 'text:numeric', 'text:float', 'text:email', 'text:tel', 'text:url', 'checkbox', 'date', 'multiselect', 'select', 'textarea', 'upload', 'custom:postalcode', 'custom:country', 'custom:bankaccount'));
