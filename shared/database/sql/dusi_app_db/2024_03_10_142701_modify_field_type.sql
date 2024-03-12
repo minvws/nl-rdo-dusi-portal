70b9d5c8feee6697582bf8cbd0a07d6eaaa7b1bd
Maurice Maas <maurice@egeniq.com>
2024-03-12 11:53:50 +0100

    DUSI-636: First drop contraint and then re-add it

--- shared/database/sql/dusi_app_db/2024_03_10_142701_modify_field_type.sql
+++ shared/database/sql/dusi_app_db/2024_03_10_142701_modify_field_type.sql
@@ -1,2 +1,4 @@
+ALTER TABLE public.fields DROP CONSTRAINT fields_type_check;
+
 ALTER TABLE public.fields
     ADD CONSTRAINT fields_type_check check ("type" in ('text', 'text:numeric', 'text:float', 'text:email', 'text:tel', 'text:url', 'checkbox', 'date', 'multiselect', 'select', 'textarea', 'upload', 'custom:postalcode', 'custom:country', 'custom:bankaccount'));
