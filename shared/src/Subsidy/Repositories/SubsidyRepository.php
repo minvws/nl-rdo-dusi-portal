0bdc4462e70ec99e7098e06c27039a048f03354f
Maurice Maas <maurice@egeniq.com>
2024-03-22 12:15:45 +0100

    DUSI-1366: Make findSubsidyByCode case insensitive

--- shared/src/Subsidy/Repositories/SubsidyRepository.php
+++ shared/src/Subsidy/Repositories/SubsidyRepository.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace MinVWS\DUSi\Shared\Subsidy\Repositories;
 
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Str;
 use MinVWS\DUSi\Shared\Subsidy\Models\Enums\SubjectRole;
 use MinVWS\DUSi\Shared\Subsidy\Models\Enums\VersionStatus;
 use MinVWS\DUSi\Shared\Subsidy\Models\Field;
 use MinVWS\DUSi\Shared\Subsidy\Models\Subsidy;
 use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyStage;
 use MinVWS\DUSi\Shared\Subsidy\Models\SubsidyVersion;
+use Illuminate\Support\Facades\DB;
 
 class SubsidyRepository
 {
@@ -211,7 +213,7 @@ class SubsidyRepository
 
     public function findSubsidyByCode(string $code): ?Subsidy
     {
-        return Subsidy::query()->where('code', '=', $code)->first();
+        return Subsidy::query()->where(DB::raw('lower(code)'), '=', Str::lower($code))->first();
     }
 
     public function getFirstStageForSubsidyVersion(SubsidyVersion $subsidyVersion): SubsidyStage
