0fd43272900fb461a79a26b7b9e11cc37553fe37
Laurence Keijmel <laurence@egeniq.com>
2024-03-25 22:27:20 +0100

    Fix migration error

--- shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
+++ shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.application_references (reference, used, deleted, created_at, updated_at)
 SELECT a.reference, true, false, a.created_at, a.created_at
-FROM applications a;
+FROM public.applications a;
