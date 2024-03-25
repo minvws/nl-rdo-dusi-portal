8c2008327c23dcbc131e0495ab0a71a2cd9b5027
Laurence Keijmel <laurence@egeniq.com>
2024-03-25 22:42:32 +0100

    Fix reference keyword

--- shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
+++ shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.application_references
 (
-    reference  varchar(15) NOT NULL,
+    "reference"  varchar(15) NOT NULL,
     used       boolean DEFAULT true NOT NULL ,
     created_at timestamp(0) without time zone,
     updated_at timestamp(0) without time zone,
@@ -10,9 +10,9 @@ CREATE TABLE public.application_references
 ALTER TABLE public.application_references OWNER TO postgres;
 
 ALTER TABLE ONLY public.application_references
-    ADD CONSTRAINT application_references_reference_unique UNIQUE (reference);
+    ADD CONSTRAINT application_references_reference_unique UNIQUE ("reference");
 
 
-INSERT INTO public.application_references (reference, used, deleted, created_at, updated_at)
-SELECT a.reference, true, false, a.created_at, a.created_at
+INSERT INTO public.application_references ("reference", used, deleted, created_at, updated_at)
+SELECT a."reference", true, false, a.created_at, a.created_at
 FROM public.applications a;
