c7139f82e28ff0f8418d6dca8a8b064b5908d96a
Laurence Keijmel <laurence@egeniq.com>
2024-03-25 22:37:14 +0100

    Add create table to migrations

--- shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
+++ shared/database/sql/dusi_app_db/2024_03_25_000000_fill_reference_table.sql
@@ -1,3 +1,18 @@
+CREATE TABLE public.application_references
+(
+    reference  varchar(15) NOT NULL,
+    used       boolean DEFAULT true NOT NULL ,
+    created_at timestamp(0) without time zone,
+    updated_at timestamp(0) without time zone,
+    deleted    boolean DEFAULT false NOT NULL
+);
+
+ALTER TABLE public.application_references OWNER TO postgres;
+
+ALTER TABLE ONLY public.application_references
+    ADD CONSTRAINT application_references_reference_unique UNIQUE (reference);
+
+
 INSERT INTO public.application_references (reference, used, deleted, created_at, updated_at)
 SELECT a.reference, true, false, a.created_at, a.created_at
 FROM public.applications a;
