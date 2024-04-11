fff72d9df24aea316f82a48fcf5f9cdf40ce1f9e
Maurice Maas <maurice@egeniq.com>
2024-04-11 14:38:12 +0200

    Code improvements

--- assessment-api/app/Console/Commands/MigrateAuditLogging.php
+++ assessment-api/app/Console/Commands/MigrateAuditLogging.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Console\PromptsForMissingInput;
 use MinVWS\DUSi\Shared\Application\Services\LetterService;
 use MinVWS\DUSi\Shared\Subsidy\Services\SubsidyFileManager;
 use MinVWS\Logging\Laravel\Models\AuditLog;
+use SplFileObject;
 
 class MigrateAuditLogging extends Command implements PromptsForMissingInput
 {
@@ -17,7 +18,7 @@ class MigrateAuditLogging extends Command implements PromptsForMissingInput
      *
      * @var string
      */
-    protected $signature = 'app:migrate-audit-logging {--their-pub-nacl-key=} {--our-priv-nacl-key=} {--audit-file=}';
+    protected $signature = 'app:migrate-audit-logging {--their-pub-nacl-key=} {--our-priv-nacl-key=} {audit-file}';
 
     /**
      * The console command description.
@@ -36,36 +37,34 @@ class MigrateAuditLogging extends Command implements PromptsForMissingInput
      */
     public function handle(): void
     {
-        $contents = file_get_contents($this->option('audit-file'));
-        $lines = explode("\n", $contents); // this is your array of words
+        if (!file_exists($this->argument('audit-file'))) {
+            $this->error('Audit logging file does not exist');
+        }
 
-        $ourPriv = base64_decode($this->option('our-priv-nacl-key'));
-        $theirPub = base64_decode($this->option('their-pub-nacl-key'));
+//        $ourPrivatKey = base64_decode($this->option('our-priv-nacl-key'));
+//        $theirPublicKey = base64_decode($this->option('their-pub-nacl-key'));
 
-        $encryption_secret = base64_decode('JOJQALLzzLVnr/mPBIzidJs/9KjN4f/wd/Y2euxGQQo=');
-        $encryption_public = base64_decode('bHTP2+EeQ1ydVYhBZsc0L1S4ANgtmR6P0FCMmxnTRVw=');
-        $decryption_secret = base64_decode('QND4gkBgBopVGGpIqcuuMbIe0n+7HAsw4p6t/R/lwyY=');
-        $decryption_public = base64_decode('x8YDbw9sEripm/qilgNLjiKEvmF+4mx3RREMrVzIPQ4=');
+        $ourPrivatKey = base64_decode('a/NBbsb96Kl0o7vGCYoWWMnvr+okSTWk7VuYPVF9PmM=');
+        $theirPublicKey = base64_decode('q7qP8+ptuihG6fmwH3xXqPqg77Or5J3RG7nda4V3Gms=');
 
         $decryption_keypair = sodium_crypto_box_keypair_from_secretkey_and_publickey(
-            $decryption_secret, $encryption_public);
+            $ourPrivatKey, $theirPublicKey);
 
-        foreach($lines as $line) {
+        $file = new SplFileObject($this->argument('audit-file'), 'r');
+
+        foreach ($file as $line) {
             $this->handleLine($line, $decryption_keypair);
         }
     }
 
     public function handleLine(string $line, $decryption_keypair): void
     {
-        $parts = explode(' ', $line);
-        if (count($parts) < 7) {
-            $this->warn("skipping: $line");
+        if (strpos($line, 'AuditLog:') === false) {
+            $this->warn("No AuditLog found: $line");
             return;
         }
 
-
-        $logIndex = array_search('AuditLog:', $parts) + 1;
-        $auditLog = $parts[$logIndex];
+        $auditLog = substr($line, strpos($line, 'AuditLog:') + 10);
 
         $encrypted = base64_decode($auditLog);
 
@@ -77,13 +76,14 @@ class MigrateAuditLogging extends Command implements PromptsForMissingInput
             $this->warn("failed to decrypt: $line");
             return;
         }
+
         $logEvent = json_decode($decrypted, true);
-//        dd(new AuditLog($logEvent));
-//        dd($logEvent);
+
         if (isset($logEvent['request'])){
             $logEvent['context'] = $logEvent['request'];
             unset($logEvent['request']);
         }
+
         $auditLog = new AuditLog($logEvent);
         $auditLog->save();
     }
