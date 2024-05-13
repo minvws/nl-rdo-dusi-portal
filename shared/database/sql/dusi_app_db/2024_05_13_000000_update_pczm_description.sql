1cf9b805368b948b8196a10596cf6d44032dd9fe
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-05-13 13:51:10 +0200

    Update PCZM description (#1741)

---
+++ shared/database/sql/dusi_app_db/2024_05_13_000000_update_pczm_description.sql
@@ -0,0 +1,3 @@
+UPDATE public.subsidies
+SET description = 'De regeling Zorgmedewerkers met langdurige post-COVID klachten richt zich op zorgmedewerkers die tijdens de eerste golf van de COVID-19 pandemie besmet zijn geraakt met COVID-19 en sindsdien langdurige post-COVID klachten hebben. Deze klachten hebben grote invloed op het werk en het privéleven van deze zorgmedewerkers. Zij kunnen soms hun eigen werk als zorgmedewerker niet meer (volledig) doen. Voor deze specifieke groep zorgmedewerkers is een eenmalige financiële ondersteuning van €24.010 beschikbaar.'
+WHERE id = '06a6b91c-d59b-401e-a5bf-4bf9262d85f8';
