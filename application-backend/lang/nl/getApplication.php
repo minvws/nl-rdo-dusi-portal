c374297fb5f3945129e4130eda2e5e8446a523cc
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-06-13 16:54:36 +0200

    Updated error messages after discussing with Heleen (#1877)

--- application-backend/lang/nl/getApplication.php
+++ application-backend/lang/nl/getApplication.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'identity_not_found_code' => 'application_not_found',
-    'identity_not_found_message' => 'Aanvraag niet gevonden.',
+    'identity_not_found_message' => 'Aanvraag kan niet geopend worden. Controleer de url en of u de juiste rechten heeft.',
     'application_not_found_code' => 'application_not_found',
-    'application_not_found_message' => 'Aanvraag niet gevonden.'
+    'application_not_found_message' => 'Aanvraag kan niet geopend worden. Controleer de url en of u de juiste rechten heeft.'
 ];
